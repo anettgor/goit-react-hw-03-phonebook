bec747a1de8397afd733d6085bf8bf1216501f33
anettgor <anetagorowska06@onet.pl>
2023-03-25 14:38:02 +0100

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
